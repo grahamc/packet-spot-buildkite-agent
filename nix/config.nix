04cfe0361b23ee0a725791ef3672f02d4d3dac5d
Graham Christensen <graham@grahamc.com>
2020-03-24 22:03:34 -0400

    nixops-packet: py3

--- nix/config.nix
+++ nix/config.nix
@@ -13,10 +13,8 @@ let
     };
 
     nixops-packet = {
-      # uncomment when PRs #3 and #4 merge and automatic CPR merges
-      #url = "https://github.com/input-output-hk/nixops-packet.git";
       url = "https://github.com/grahamc/nixops-packet.git";
-      branch = "auto-cpr";
+      branch = "py3";
     };
   };
 
