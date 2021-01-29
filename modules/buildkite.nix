644f2aac48d58d3b9c559b4e957447261edbf35b
Graham Christensen <graham@grahamc.com>
2021-01-29 13:59:13 -0500

    fixup modules/buildkite.nix: missing }

--- modules/buildkite.nix
+++ modules/buildkite.nix
@@ -11,19 +11,20 @@
         action = [ "sudo" "systemctl" "restart" "buildkite-agent-r13y.service" ];
       };
     };
+  };
 
-    services.buildkite-agents.r13y = {
-      enable = true;
-      tags = {
-        r13y = "true";
-        packet-spot = "true";
-      };
-      tokenPath = "/run/keys/buildkite-token";
-      privateSshKeyPath = "/dev/null";
-      runtimePackages = [ pkgs.xz pkgs.gzip pkgs.gnutar pkgs.gitAndTools.git-crypt pkgs.nix pkgs.bash ];
-      hooks.environment = ''
-        export PATH=$PATH:/run/wrappers/bin/
-        export NIX_PATH=nixpkgs=${pkgs.path}
-      '';
+  services.buildkite-agents.r13y = {
+    enable = true;
+    tags = {
+      r13y = "true";
+      packet-spot = "true";
     };
-  }
+    tokenPath = "/run/keys/buildkite-token";
+    privateSshKeyPath = "/dev/null";
+    runtimePackages = [ pkgs.xz pkgs.gzip pkgs.gnutar pkgs.gitAndTools.git-crypt pkgs.nix pkgs.bash ];
+    hooks.environment = ''
+      export PATH=$PATH:/run/wrappers/bin/
+      export NIX_PATH=nixpkgs=${pkgs.path}
+    '';
+  };
+}
