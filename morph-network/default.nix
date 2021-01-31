bb6792cf46592c1ab71b3179674789c14d1c64a8
Buildkite <ci@buildkite>
2021-01-31 21:23:59 +0000

    Automatic update of deploy targets.

--- morph-network/default.nix
+++ morph-network/default.nix
@@ -7,4 +7,16 @@
     };
   };
 
+  "buildkite-spot-0" = {
+    deployment = {
+      targetHost = "147.75.199.51";
+      targetUser = "root";
+      substituteOnDestination = true;
+    };
+    imports = [
+      ../modules
+      ./machines/buildkite-spot-0.expr.nix
+      ./machines/buildkite-spot-0.system.nix
+    ];
+  };
 }
