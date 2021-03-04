db34f09b42f019c2ace6ca6cc93a0e467867cbc3
Graham Christensen <graham@grahamc.com>
2021-03-04 08:38:04 -0500

    Run GC every hour
    Hopefully this doesn't break r13y

--- modules/nix-collect-garbage.nix
+++ modules/nix-collect-garbage.nix
@@ -2,7 +2,7 @@
   # clean up time
   nix.gc = {
     automatic = true;
-    dates = "daily";
+    dates = "hourly";
     options = let gbFree = 2000; in "--max-freed $((${toString gbFree} * 1024**3 - 1024 * $(df -P -k /nix/store | tail -n 1 | ${pkgs.gawk}/bin/awk '{ print $4 }')))";
   };
   systemd.tmpfiles.rules = [''
